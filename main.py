37adad404b00d4d465853afcc216d53907644b1f
frostforestcdjl <49707220+frostforestcdjl@users.noreply.github.com>
2019-06-13 13:06:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np
 import math
 import random
 import time
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation as animation
 
 #--------------------------------------------------------------------
 # parameters
